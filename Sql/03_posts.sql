16f8180447a777b37cbcf4d2903c2ba263767fa8
Frank Hellenkamp <jonas@depage.net>
2017-03-21 18:01:04 +0100

    updated database scheme for post voting in discuss

--- Sql/03_posts.sql
+++ Sql/03_posts.sql
@@ -14,8 +14,6 @@ CREATE TABLE `_discuss_posts` (
     `post` text NOT NULL,
     `postDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `editDate` timestamp NULL DEFAULT NULL,
-    `upvotes` smallint(2) unsigned NOT NULL DEFAULT '0',
-    `downvotes` smallint(2) unsigned NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`),
     CONSTRAINT `_discuss_posts_ibfk_1` FOREIGN KEY (`threadId`) REFERENCES `_discuss_threads` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `_discuss_posts_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `_auth_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
---
+++ Sql/04_post_votes.sql
@@ -0,0 +1,19 @@
+/*
+    Post votes Table
+    -----------------------------------
+
+    @tablename _discuss_votes
+    @connection _discuss_posts
+    @connection _auth_user
+    @version 1.5.6
+*/
+CREATE TABLE `_discuss_votes` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `postId` int(11) unsigned NOT NULL,
+    `uid` int(11) unsigned NOT NULL,
+    `vote` smallint(2) NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    CONSTRAINT `_discuss_votes_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `_discuss_posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `_discuss_votes_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `_auth_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
